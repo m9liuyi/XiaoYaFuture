695a4cdaed5c07e55129c317128e6142e42bda16
liuyimac <>
2018-12-16 22:44:52 +0800

    引入layui 1. import layui from 'layui-src' 报错 2. 从官网下载 https://res.layui.com/static/download/layui/layui-v2.4.5.zip 解压后黏贴至static 目录下，然后在页面中使用

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// import Layui from 'layui-src'
+
 Vue.use(Router)
 
 export default new Router({
